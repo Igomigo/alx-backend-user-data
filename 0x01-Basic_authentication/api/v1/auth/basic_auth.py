1920359820d2d8574f7cb4d7c357e351fb960df9
Igomigo <igomigofatai@gmail.com>
2024-01-23 03:40:11 +0000

    Basic auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" contains the basic auth class """
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ The basic auth class that inherits from the Auth class """
+    pass
