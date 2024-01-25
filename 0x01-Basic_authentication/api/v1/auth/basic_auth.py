ab700b9ee846c581a55600567d34c916f61b77b2
Igomigo <igomigofatai@gmail.com>
2024-01-25 00:15:34 +0000

    base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,12 +5,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ The basic auth class that inherits from the Auth class """
-    
+
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """ returns the authentication value after Basic """
         if authorization_header is None or\
-            not isinstance(authorization_header, str):
+                not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith("Basic "):
             return None
