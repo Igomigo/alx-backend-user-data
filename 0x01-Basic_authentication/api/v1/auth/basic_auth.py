c9ca93666ef15b6011530cf43313398f2c20d492
Igomigo <igomigofatai@gmail.com>
2024-01-25 06:31:56 +0000

    retrieved user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,11 +26,24 @@ class BasicAuth(Auth):
             return None
 
         try:
-            #encoded = base64_authorization_header.encode('utf-8')
-            #decoded64 = b64decode(encoded)
+            # encoded = base64_authorization_header.encode('utf-8')
+            # decoded64 = b64decode(encoded)
             decoded64 = b64decode(base64_authorization_header)
             decoded = decoded64.decode('utf-8')
         except BaseException:
             return None
 
         return decoded
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ returns the user email and password """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        email, passwd = decoded_base64_authorization_header.split(":")
+        return (email, passwd)
