d6972e7493b7acc9344f2092406419972b21823d
Igomigo <igomigofatai@gmail.com>
2024-01-30 18:44:22 +0000

    Retrieve session cookie from request

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -35,3 +35,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ returns None """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request """
+        if request is None:
+            return None
+        session_name = getenv("SESSION_NAME")
+        return request.cookies.get(session_name)
