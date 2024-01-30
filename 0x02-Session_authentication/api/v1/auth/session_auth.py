baf7d07ffbb2790d864531ee6749af9a2c9b13d3
Igomigo <igomigofatai@gmail.com>
2024-01-30 17:43:48 +0000

    session auth class updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ contains the session auth class
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -11,7 +12,7 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """ Creates the session ID for a user """
-        if user_id is None and not isinstance(user_id, str):
+        if user_id is None or not isinstance(user_id, str):
             return None
         session_id: str = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
