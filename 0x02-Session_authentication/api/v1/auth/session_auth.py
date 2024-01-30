273ed865c4559ccfdb55195aa7bf145d85b9ac83
Igomigo <igomigofatai@gmail.com>
2024-01-30 04:00:09 +0000

    Create session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,3 +7,12 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     """ The Session auth class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates the session ID for a user """
+        if user_id is None and not isinstance(user_id, str):
+            return None
+        session_id: str = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
