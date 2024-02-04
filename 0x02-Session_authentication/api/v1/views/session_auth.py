fc398783abff53501908ea44189dd6f81f877b64
Igomigo <igomigofatai@gmail.com>
2024-02-04 02:11:43 +0000

    session authentication set

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -7,8 +7,7 @@ from api.v1.views import app_views
 from flask import request, jsonify, make_response
 
 
-@app_views.route("/auth_session/login", methods=['POST'],
-                 strict_slashes=False)
+@app_views.route("/auth_session/login", methods=['POST'], strict_slashes=False)
 def session_login():
     """ Handles user login session """
     email = request.form.get("email")
@@ -28,4 +27,4 @@ def session_login():
             response = make_response(jsonify({ user.to_json() }))
             response.set_cookie(session_name, session_id)
             return response
-        return jsonify({"error": "wrong password"}), 401
+    return jsonify({"error": "wrong password"}), 401
