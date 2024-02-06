901a4943b6552cd4dae3f80cc8074dc60b67dd67
Igomigo <igomigofatai@gmail.com>
2024-02-06 16:53:53 +0000

    hashed password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,15 +2,11 @@
 """ Contains the authentication systems
 """
 import bcrypt
-from typing import Optional
 
 
-salt = bcrypt.gensalt()
-
-
-def _hash_password(password: str) -> Optional[bytes]:
+def _hash_password(password: str) -> str:
     """ Method hashes a password """
     if password is None:
         return None
-    hashed = bcrypt.hashpw(password.encode(), salt)
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
