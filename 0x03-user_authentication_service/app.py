631df1d3c54c792b0b23dc9a1029d1c0ec5b4579
Igomigo <igomigofatai@gmail.com>
2024-02-08 21:17:12 +0000

    user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,17 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile", methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ returns a user profile based on the value of the session_id """
+    session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": f"{user.email}"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
