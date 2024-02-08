34f58ac852aa0b9b24c62d5ffcbdbe2b443853ee
Igomigo <igomigofatai@gmail.com>
2024-02-08 05:37:02 +0000

    Register User Route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,16 +15,19 @@ def message() -> str:
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def user() -> str:
     """ Registers a user """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if email is None or password is None:
-        return "No email and password"
     try:
-        AUTH.register_user(email, password)
-        message = {"email": email, "message": "user created"}
-        return jsonify(message), 200
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
 
 
 if __name__ == "__main__":
